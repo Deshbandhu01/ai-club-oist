6c8ba95ae8d100bd216a5e408b20642112d2c6e9
Deshbandhu01 <dbadhualiya@gmail.com>
2025-03-25 14:51:05 +0530

    first commit

--- src/pages/Contact_page/Contact_p.jsx
+++ src/pages/Contact_page/Contact_p.jsx
@@ -1,47 +1,16 @@
 import React, { useState } from 'react';
 import "./Contact_p.css"
 
-const Contact_p = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    college: '',
-    year: '',
-    skills: ''
-  });
-
-  const [result, setResult] = useState('');
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission
-    
-    const { name, email, college, year, skills } = formData;
-
-    // Display success message
-    setResult(`
-      <h3>Thank you for registering!</h3>
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>College: ${college}</p>
-      <p>Year: ${year}</p>
-      <p>Skills: ${skills}</p>
-    `);
-  };
+function Contact_p(){
 
   return (
     <>
     <div className="link_name">/ Contact </div>
     <div className="contact_sec">
     {/* <h1>Connect with us!</h1> */}
+    <div className="Form_cont">
     <iframe src="https://forms.visme.co/formsPlayer/rxw30wp0-untitled-project" className="Contact_form"> </iframe>
+    </div>
     </div>
     
     </>
