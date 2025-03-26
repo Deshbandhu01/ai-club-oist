7c434a0894b853aada680b40ef6a25c296583f7f
Deshbandhu01 <dbadhualiya@gmail.com>
2025-03-26 12:29:22 +0530

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar/Navbar.jsx"
 // import Footer from "./components/Footer/Footer.jsx"
 import Hero from "./components/Hero/Hero.jsx"
 import About from "./components/body/about/About.jsx"
-import Event from "./components/body/events/event.jsx";
+import Event from "./components/body/events/Event.jsx";
 import Resource from "./components/body/Resources/Resouce.jsx";
 import Blog from "./components/body/Blog/Blog.jsx";
 import Team from "./components/body/Team/Team.jsx";
